fe4e4bc14bb7a2038383cb57b484d45f1f4e8785
Kimchanghee <9594198+Kimchanghee@users.noreply.github.com>
2026-06-11 02:42:09 +0900

    Switch MacroFinanceNews GA4 tag to site property

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html>
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3LYD80FR0D" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZNTZPPNYV0" />
         <script
           dangerouslySetInnerHTML={{
-            __html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-3LYD80FR0D',{page_path:window.location.pathname});",
+            __html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-ZNTZPPNYV0',{page_path:window.location.pathname});",
           }}
         />
       </head>
